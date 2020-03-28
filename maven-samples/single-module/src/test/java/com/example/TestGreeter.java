220427b39956c6975ad03c6d20333e7b3967f8ab
jasonlutzphx <jasonlutzphx@gmail.com>
2020-03-28 16:03:48 -0700

    Renamed the redundant module.

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -39,7 +39,7 @@ public class TestGreeter {
   }
 
     @Test
-  public void greetShouldIncludeGreetingMessage() {
+  public void greetShouldIncludeGreetingPandemic() {
     String someone = "World, Sorry about covit-19";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
